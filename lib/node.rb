ad2b88deb55f6302af38b2e1df4c33d5fa6f8ba0
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-09-09 14:32:37 +0100

    Creted project structure Added Tree class to implement a balanced BST Added Node class to implement a tree's node

---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# a Ruby implementation of a node of a balanced BST
+class Node
+  def initialize
+    @value = ''
+    @left = nil
+    @right = nil
+  end
+end
---
+++ lib/tree.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# a Ruby implementation of a balanced BST
+class BalancedBST
+  def initialize
+    @root = ''
+    @nodes = []
+  end
+end
